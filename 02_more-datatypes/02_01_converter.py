4d3360e9d16cf3cea2828261c4bb1e59ed94847f
Amber <adm2037@gmail.com>
2024-07-16 16:31:25 -0500

    Compared str vs tuple behavior

--- 02_more-datatypes/02_01_converter.py
+++ 02_more-datatypes/02_01_converter.py
@@ -4,3 +4,11 @@
 # Do you notice any difference to iterating over the string?
 
 string = "codingnomads"
+for char in string:
+    print(char) # prints each individual character on new line
+
+tup = ('coding', 'nomads')
+for char in tup:
+    print(char) # prints each word on new line
+
+
