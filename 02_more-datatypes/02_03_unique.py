9a23f2751ab019948d6f45a1e8c34ab1aadfa83c
Amber <adm2037@gmail.com>
2024-07-16 16:51:14 -0500

    Identified unique elements in a list

--- 02_more-datatypes/02_03_unique.py
+++ 02_more-datatypes/02_03_unique.py
@@ -3,3 +3,7 @@
 #
 # list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 # unique_list = [55, 'hi', 4, 13]
+
+festivals = ['Euphoria', 'Art Outside', 'LIB', 'EDCLV', 'EF', 'LIB', 'Euphoria', 'Lostlands', 'Freaky Deaky']
+unique = set(festivals)
+print(unique)
