657812a35438edc36ec01ca1939c977b13397666
Amber <adm2037@gmail.com>
2024-07-23 16:29:38 -0500

    Used Counter subclass to obtain most frequent word from user input

--- 02_more-datatypes/02_05_split_and_pick.py
+++ 02_more-datatypes/02_05_split_and_pick.py
@@ -2,9 +2,18 @@
 # Using the `split()` method, create a list of all the words in the string
 # and print back the most common word in the text.
 
+from collections import Counter
+
 message = input("What's your favorite movie and why? ")
 
 # Split message into individual words
 words = message.split()
 print(words)
 
+# Get frequency of each word
+Counter = Counter(words)
+
+# Passing 1 to get single most frequent word (could also pass another number here)
+# as well as times the word was used
+most_freq = Counter.most_common(1)
+print(most_freq)
