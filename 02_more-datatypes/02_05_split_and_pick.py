c4da8f749d7aed40ebc8ea014d3bcd0447a545d2
Amber <adm2037@gmail.com>
2024-07-23 16:22:54 -0500

    Split user input into individual words first

--- 02_more-datatypes/02_05_split_and_pick.py
+++ 02_more-datatypes/02_05_split_and_pick.py
@@ -1,3 +1,10 @@
 # Write a script that takes in a string from the user.
 # Using the `split()` method, create a list of all the words in the string
 # and print back the most common word in the text.
+
+message = input("What's your favorite movie and why? ")
+
+# Split message into individual words
+words = message.split()
+print(words)
+
