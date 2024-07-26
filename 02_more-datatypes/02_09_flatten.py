9582c39ae9b18093cdc3f6ad6ab4e7e467fb3f3a
Amber <adm2037@gmail.com>
2024-07-25 20:54:41 -0500

    Need to debug typeerror on lines 16-18

--- 02_more-datatypes/02_09_flatten.py
+++ 02_more-datatypes/02_09_flatten.py
@@ -10,3 +10,15 @@
 # to flatten a list of any depth. Can you understand the code used?
 
 starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+
+# Easier to convert to set to use set operations
+# try indexing lists
+a = {starter_list[0]} # TypeError: unhashable type: 'list'
+b = {starter_list[1]}
+c = {starter_list[2]}
+
+# Use union to concatenate lists
+flatten = a|b|c
+
+# Convert to list and returned flattened version
+print (list(flatten))
