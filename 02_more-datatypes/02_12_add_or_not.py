42e198caa10da8000e65ec78124243f445334c34
Amber <adm2037@gmail.com>
2024-07-25 21:36:16 -0500

    Nearly finished creating game -- need to resolve a small bug in losing case and a TypeError on line 46

--- 02_more-datatypes/02_12_add_or_not.py
+++ 02_more-datatypes/02_12_add_or_not.py
@@ -6,3 +6,45 @@
 # their input is a duplicate and deduct a point.
 # If the user loses 5 points, quit the program.
 # They win if they manage to create a set that has more than 10 items.
+
+# Initialize empty set of number entries
+num_ = set()
+
+# Initialize duplicate number count
+points = 5
+
+while True:
+    # Prompt user for number
+    num = input("Please enter number here: ")
+
+    # Rules for adding numbers to set
+    if (num not in num_) and (points!=0):
+        # Syntax similar to append: set.add(elem)
+        # try except to catch errors aka can't be converted
+        try:
+            int(num)
+        except TypeError:
+            print(f"{num} could not be converted to int.")
+            break
+        # If could be converted to int continue loop
+        num_.add(num)
+    elif (points == 0):
+        # TODO: losing eventually works but continues one entry after loss -- FIX
+        print("Uh oh, you lost!")
+        break
+    else:
+        # Prevent duplicate entries
+        print("This number already exists in the set.")
+        # Deduct points by 1
+        points -= 1
+    # Continue loop until 5 points have been deducted
+    continue
+
+# Decide winners
+for i in num_:
+    # TODO: Also resolve TypeError below
+    if i > 10: # TypeError: '>' not supported between instances of 'str' and 'int'
+        print("Congratulations, the set is larger than 10 numbers so you WIN!")
+
+# Verify number of items in set
+print(num_)
