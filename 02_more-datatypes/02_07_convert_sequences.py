90cecd780ea3154b413285c5b760914ad55b017f
Amber <adm2037@gmail.com>
2024-07-25 20:50:35 -0500

    Finished all changes and type conversions

--- 02_more-datatypes/02_07_convert_sequences.py
+++ 02_more-datatypes/02_07_convert_sequences.py
@@ -5,3 +5,21 @@
 # - Convert the list back into a tuple.
 
 string = "codingnomads"
+
+# String → tuple
+tup = tuple(string)
+
+# Verify tuple
+print(tup)
+
+# Tuple → list
+ls = list(tup)
+print(ls)
+
+# c → k
+ls[0] = "k"
+# Verify change
+print(ls)
+
+# List → tuple and display result
+print(tuple(ls))
