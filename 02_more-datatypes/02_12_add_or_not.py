3f9651777f651a25a7bad240b643d47f6f0d53e7
Amber <adm2037@gmail.com>
2024-07-31 19:23:20 -0500

    Fixed issues with game by unnesting some of the if/elif/else statements -- running normally now

--- 02_more-datatypes/02_12_add_or_not.py
+++ 02_more-datatypes/02_12_add_or_not.py
@@ -7,46 +7,32 @@
 # If the user loses 5 points, quit the program.
 # They win if they manage to create a set that has more than 10 items.
 
-# Initialize empty set of number entries
+# Initialize empty set for storing number inputs
 num_ = set()
 
-# Initialize duplicate number count
+# Initialize points
 points = 5
 
-# Set rules for adding user's number inputs to the set or not
-while True:
-    # Prompt user for number
-    num = input("Please enter number here: ")
-
-    # Rules for adding numbers to set
-    # if (num not in num_) and (points!=0): # Refactoring this line to see if it fixes 
-    # the only stopping after 0 points instead of winning
-    if (num not in num_):
-        # Syntax similar to append: set.add(elem)
-        # try except to catch errors aka can't be converted
-        try:
-            int(num)
-        except TypeError:
-            print(f"{num} could not be converted to int.")
-            break
-        # Continue loop if num can be converted to int
+while len(num_) <= 10:
+    # Prompt user for numbers
+    num = input("Please enter a random number here: ")
+    # Add number to set if not already in it
+    if num not in num_:
         num_.add(num)
     else:
-        # Prevent duplicate entries
         print("This number already exists in the set.")
         # Deduct points by 1
         points -= 1
-    # Continue loop until 5 points have been deducted
+        print(points)
+    # Determine losers
+    if (points == 0):
+        print("Uh oh, you lost!")
+        break
+    # Determine winners
+    if (len(num_) > 10): 
+        print("Congratulations, you win!")
+        break
     continue
-
-# TODO: Moving this section (lines 42-49) out from earlier loop broke winning/losing altogether -- FIX
-# Determine losers
-if (points == 0):
-    print("Uh oh, you lost!")
-
-# Determine winners
-if (len(num_) > 10): 
-    print("Congratulations, the set is larger than 10 numbers so you WIN!")
 
 # Verify number of items in set
 print(num_)
