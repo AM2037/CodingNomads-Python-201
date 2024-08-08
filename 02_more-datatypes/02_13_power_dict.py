e171a37a77d4b7583e5d30249ea0ce2a146a3a44
Amber <adm2037@gmail.com>
2024-08-08 16:14:02 -0500

    Created dictionary from range with for loop

--- 02_more-datatypes/02_13_power_dict.py
+++ 02_more-datatypes/02_13_power_dict.py
@@ -1,3 +1,13 @@
 # Write a script that creates a dictionary of keys, `n`
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
+
+keys_dict = {}
+
+# Define dictionary keys & values
+for key in range(1,11):
+    v = key * key
+    keys_dict[key] = v
+
+# Print resulting dictionary
+print(keys_dict)
