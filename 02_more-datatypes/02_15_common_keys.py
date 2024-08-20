234b6527cf99cc34f8921c0628c2deea78748b0d
Amber <adm2037@gmail.com>
2024-08-20 17:23:53 -0500

    Used a combo of loop and dictionary comprehensions to sum over multiple values of duplicate (or common) keys

--- 02_more-datatypes/02_15_common_keys.py
+++ 02_more-datatypes/02_15_common_keys.py
@@ -6,6 +6,19 @@
 #
 # Example output:
 # result = {"a": 3, "b": 2, "c": 7 , "d": 2}
+from collections import defaultdict
 
 dict_1 = {"a": 1, "b": 2, "c": 3}
 dict_2 = {"a": 2, "c": 4 , "d": 2}
+
+def_dict = defaultdict(list)
+
+# First list multiple values from each dictionary for duplicate keys
+for d in (dict_1, dict_2):
+    [def_dict[key].append(value) for key,value in d.items()]
+    # Add duplicate values
+    common_keys = {key: [sum(def_dict[key])] for key in def_dict.keys()}
+
+print(common_keys)
+
+
